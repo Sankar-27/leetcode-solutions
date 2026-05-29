645f20027b04161797ce316e4be235a34d8a9fa7
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:56:14 +0530

    Create 561-ArrayPartition

---
+++ https:/github.com/Sankar-27/leetcode-solutions/561-ArrayPartition/561-ArrayPartition.java
@@ -0,0 +1,13 @@
+// Last updated: 29/05/2026, 11:56:32
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int maxSum = 0;
+
+        for(int i=0; i<nums.length; i=i+2) {
+            maxSum += nums[i];
+        }
+
+        return maxSum;
+    }
+}
