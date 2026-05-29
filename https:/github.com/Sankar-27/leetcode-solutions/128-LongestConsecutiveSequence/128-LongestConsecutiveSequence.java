550aea8978c2c547ba6041cda9a878a623c06870
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:58:57 +0530

    Create 128-LongestConsecutiveSequence

---
+++ https:/github.com/Sankar-27/leetcode-solutions/128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
@@ -0,0 +1,28 @@
+// Last updated: 29/05/2026, 11:59:15
+class Solution {
+    public int longestConsecutive(int[] nums) {
+
+        int n = nums.length;
+        if (n == 0) return 0;
+        int longestLength = 1;
+
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(nums[i]);
+        }
+
+        for (int i : set) {
+            if (!set.contains(i - 1)) {
+                int count = 1;
+                int x = i;
+                while (set.contains(x + 1)) {
+                    x++;
+                    count++;
+                }
+                longestLength = Math.max(count, longestLength);
+            }
+        }
+
+        return longestLength;
+    }
+}
