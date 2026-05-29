66600f337b9f3330e61514a975831979c8fb6ed1
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:56:31 +0530

    Create 498-DiagonalTraverse

---
+++ https:/github.com/Sankar-27/leetcode-solutions/498-DiagonalTraverse/498-DiagonalTraverse.java
@@ -0,0 +1,23 @@
+// Last updated: 29/05/2026, 11:56:49
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m=mat.length;
+        int n=mat[0].length;
+        int res[]=new int[m*n];
+        int row=0,col=0;
+        for(int i=0;i<res.length;i++){
+            res[i] = mat[row][col];
+
+            if ((row + col) % 2 == 0) {
+                if (col == n - 1) row++;
+                else if (row == 0) col++;
+                else { row--; col++; }
+            } else {
+                if (row == m - 1) col++;
+                else if (col == 0) row++;
+                else { row++; col--; }
+            }
+        }
+        return res;
+    }
+}
