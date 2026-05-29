2bb5d3c7c5087da9a1d1a4ad30e87f8126b4c6e1
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:55:24 +0530

    Create 832-BinaryTreePruning

---
+++ https:/github.com/Sankar-27/leetcode-solutions/832-BinaryTreePruning/832-BinaryTreePruning.java
@@ -0,0 +1,32 @@
+// Last updated: 29/05/2026, 11:55:42
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        return dfs(root);
+    }
+
+    public TreeNode dfs(TreeNode root){
+        if(root==null)  return root;   
+        
+        root.left=dfs(root.left);
+        root.right=dfs(root.right);
+        
+        if(root.val==0 && root.left==null && root.right==null)
+            return null; 
+        return root;        
+    }
+}
