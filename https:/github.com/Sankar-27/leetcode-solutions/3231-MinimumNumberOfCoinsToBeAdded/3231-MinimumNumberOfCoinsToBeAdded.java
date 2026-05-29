14a6b56a6d446686acfd870de6acbdf1baf2d108
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:53:16 +0530

    Create 3231-MinimumNumberOfCoinsToBeAdded

---
+++ https:/github.com/Sankar-27/leetcode-solutions/3231-MinimumNumberOfCoinsToBeAdded/3231-MinimumNumberOfCoinsToBeAdded.java
@@ -0,0 +1,22 @@
+// Last updated: 29/05/2026, 11:53:34
+class Solution {
+    public int minimumAddedCoins(int[] coins, int target) {
+        Arrays.sort(coins);
+
+        int currentMax = 0;
+        int additions = 0;
+        int index = 0;
+
+        while (currentMax < target) {
+            if (index < coins.length && coins[index] <= currentMax + 1) {
+                currentMax += coins[index];
+                index++;
+            } else {
+                currentMax += currentMax + 1;
+                additions++;
+            }
+        }
+
+        return additions;
+    }
+}
