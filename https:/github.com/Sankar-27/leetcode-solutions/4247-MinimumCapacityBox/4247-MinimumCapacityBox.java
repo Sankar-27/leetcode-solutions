cc480f9d3a6da0a427e08fce84031d63b167eb33
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:53:01 +0530

    Create 4247-MinimumCapacityBox

---
+++ https:/github.com/Sankar-27/leetcode-solutions/4247-MinimumCapacityBox/4247-MinimumCapacityBox.java
@@ -0,0 +1,16 @@
+// Last updated: 29/05/2026, 11:53:19
+class Solution {
+    public int minimumIndex(int[] capacity, int itemSize) {
+        int minCapacity = Integer.MAX_VALUE;
+        int index = -1;
+
+        for (int i = 0; i < capacity.length; i++) {
+            if (capacity[i] >= itemSize && capacity[i] < minCapacity) {
+                minCapacity = capacity[i];
+                index = i;
+            }
+        }
+
+        return index;
+    }
+}
