69975f7a18f37db455129ae23bad653ce593f63b
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:58:11 +0530

    Create 203-RemoveLinkedListElements

---
+++ https:/github.com/Sankar-27/leetcode-solutions/203-RemoveLinkedListElements/203-RemoveLinkedListElements.java
@@ -0,0 +1,29 @@
+// Last updated: 29/05/2026, 11:58:29
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head==null)
+        return head;
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode curr = dummy;
+        while( curr !=null && curr.next!=null){
+            if(curr.next.val==val){
+                curr.next=curr.next.next;
+            }
+            else{
+                curr=curr.next;
+            }
+        }
+        return dummy.next;
+    }
+}
