9e4442cbe4abf77e66d47e7f553588e076ce14cd
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:59:16 +0530

    Create 104-MaximumDepthOfBinaryTree

---
+++ https:/github.com/Sankar-27/leetcode-solutions/104-MaximumDepthOfBinaryTree/104-MaximumDepthOfBinaryTree.java
@@ -0,0 +1,24 @@
+// Last updated: 29/05/2026, 11:59:34
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        int m = maxDepth(root.left);
+        int n = maxDepth(root.right);
+        return 1+Math.max(m,n);
+    }
+}
