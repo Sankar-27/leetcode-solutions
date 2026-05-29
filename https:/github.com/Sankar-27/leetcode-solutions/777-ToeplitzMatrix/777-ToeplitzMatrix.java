e0f5803c63b34808198c29a40ad6f01c40294e03
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:55:37 +0530

    Create 777-ToeplitzMatrix

---
+++ https:/github.com/Sankar-27/leetcode-solutions/777-ToeplitzMatrix/777-ToeplitzMatrix.java
@@ -0,0 +1,13 @@
+// Last updated: 29/05/2026, 11:55:55
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[i].length;j++){
+                if(matrix[i][j]!=matrix[i-1][j-1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
