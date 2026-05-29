227a849fd64d6b4fede7d82e48d67b8145d17f53
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:58:45 +0530

    Create 145-BinaryTreePostorderTraversal

---
+++ https:/github.com/Sankar-27/leetcode-solutions/145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.java
@@ -0,0 +1,30 @@
+// Last updated: 29/05/2026, 11:59:03
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> res= new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        traverse(root);
+        return res;
+    }
+    public void traverse(TreeNode root){
+        if(root==null)
+            return;
+        traverse(root.left);
+        traverse(root.right);
+        res.add(root.val);   
+    }
+}
