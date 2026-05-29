1c8eb052567eb028b979f1203b5db2c9c97578db
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:58:28 +0530

    Create 164-MaximumGap

---
+++ https:/github.com/Sankar-27/leetcode-solutions/164-MaximumGap/164-MaximumGap.java
@@ -0,0 +1,20 @@
+// Last updated: 29/05/2026, 11:58:46
+import java.util.*;
+
+class Solution {
+
+    public int maximumGap(int[] nums) {
+
+        if (nums.length < 2)
+            return 0;
+
+        Arrays.sort(nums);
+
+        int maxGap = 0;
+
+        for (int i = 1; i < nums.length; i++)
+            maxGap = Math.max(maxGap, nums[i] - nums[i - 1]);
+
+        return maxGap;
+    }
+}
