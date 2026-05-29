fa752cf330e3b94df2f91762e51d909a2b1c0d73
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:56:02 +0530

    Create 678-ValidParenthesisString

---
+++ https:/github.com/Sankar-27/leetcode-solutions/678-ValidParenthesisString/678-ValidParenthesisString.java
@@ -0,0 +1,28 @@
+// Last updated: 29/05/2026, 11:56:20
+class Solution {
+    public boolean checkValidString(String s) {
+     
+        int bal = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '*') {
+                bal++;
+            } else {
+                bal--;
+            }
+            if (bal < 0) return false;
+        }
+
+      
+        bal = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ')' || s.charAt(i) == '*') {
+                bal++;
+            } else {
+                bal--;
+            }
+            if (bal < 0) return false; 
+        }
+
+        return true;
+    }
+}
