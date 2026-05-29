bda7acf1d0f133aba4777e73b0e764c60fe8ce26
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:57:29 +0530

    Create 287-FindTheDuplicateNumber

---
+++ https:/github.com/Sankar-27/leetcode-solutions/287-FindTheDuplicateNumber/287-FindTheDuplicateNumber.java
@@ -0,0 +1,25 @@
+// Last updated: 29/05/2026, 11:57:47
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        int slow2 = nums[0];
+
+        while (slow != slow2) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+
+        return slow;        
+    }
+}
