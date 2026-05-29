5aaa59f6656a6756e79891bce64ca9953ede02f3
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:57:14 +0530

    Create 338-CountingBits

---
+++ https:/github.com/Sankar-27/leetcode-solutions/338-CountingBits/338-CountingBits.java
@@ -0,0 +1,10 @@
+// Last updated: 29/05/2026, 11:57:32
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
+    }
+}
