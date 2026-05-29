66f27e8a290bf47b8bf1e15da9efb243a5b2375f
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 12:00:03 +0530

    Create 49-GroupAnagrams

---
+++ https:/github.com/Sankar-27/leetcode-solutions/49-GroupAnagrams/49-GroupAnagrams.java
@@ -0,0 +1,20 @@
+// Last updated: 29/05/2026, 12:00:21
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            
+            map.get(sortedWord).add(word);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
