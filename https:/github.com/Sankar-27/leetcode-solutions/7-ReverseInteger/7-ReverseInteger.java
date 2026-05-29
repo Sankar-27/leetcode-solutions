8b6ef498e48b65c8363cd54dc9f56df9bed9310a
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 12:00:38 +0530

    Create 7-ReverseInteger

---
+++ https:/github.com/Sankar-27/leetcode-solutions/7-ReverseInteger/7-ReverseInteger.java
@@ -0,0 +1,15 @@
+// Last updated: 29/05/2026, 12:00:56
+class Solution {
+    public int reverse(int x) {
+        int revNum = 0;
+        while(x != 0){
+            int ld = x % 10;
+            if(revNum > Integer.MAX_VALUE / 10 || revNum < Integer.MIN_VALUE / 10 ){
+                return 0;
+            }
+            revNum = (revNum * 10) + ld;
+            x = x / 10;
+        }
+        return revNum;
+    }
+}
