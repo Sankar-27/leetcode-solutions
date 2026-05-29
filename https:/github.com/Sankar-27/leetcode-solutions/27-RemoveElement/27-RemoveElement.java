f7ff401028cb2593bbac67042971db984c724c37
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 12:00:18 +0530

    Create 27-RemoveElement

---
+++ https:/github.com/Sankar-27/leetcode-solutions/27-RemoveElement/27-RemoveElement.java
@@ -0,0 +1,13 @@
+// Last updated: 29/05/2026, 12:00:36
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
