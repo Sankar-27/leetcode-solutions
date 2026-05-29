ebd4726f8a8a9b2aee5565c0626b64e85a6befa5
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-05-29 11:58:50 +0530

    Create 141-LinkedListCycle

---
+++ https:/github.com/Sankar-27/leetcode-solutions/141-LinkedListCycle/141-LinkedListCycle.java
@@ -0,0 +1,25 @@
+// Last updated: 29/05/2026, 11:59:08
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head,fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)
+                return true;
+        }
+        return false;
+        
+    }
+}
