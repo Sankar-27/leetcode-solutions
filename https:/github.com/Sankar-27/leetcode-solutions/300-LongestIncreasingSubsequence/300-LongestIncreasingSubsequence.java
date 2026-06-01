fec50ab6231e960fe8b91b92c976c758c062b6f9
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-06-01 12:30:29 +0530

    File updated at 01/06/2026, 12:30:30

--- https:/github.com/Sankar-27/leetcode-solutions/300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ https:/github.com/Sankar-27/leetcode-solutions/300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,35 +1,35 @@
-// Last updated: 29/05/2026, 11:57:40
-class Solution {
-    public int lengthOfLIS(int[] nums) {
-        List<Integer> res = new ArrayList<>();
-
-        for (int n : nums) {
-            if (res.isEmpty() || res.get(res.size() - 1) < n) {
-                res.add(n);
-            } else {
-                int idx = binarySearch(res, n);
-                res.set(idx, n);
-            }
-        }
-
-        return res.size();        
-    }
-
-    private int binarySearch(List<Integer> arr, int target) {
-        int left = 0;
-        int right = arr.size() - 1;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr.get(mid) == target) {
-                return mid;
-            } else if (arr.get(mid) > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return left;
-    }    
-}
+// Last updated: 01/06/2026, 12:30:30
+1class Solution {
+2    public int lengthOfLIS(int[] nums) {
+3        List<Integer> res = new ArrayList<>();
+4
+5        for (int n : nums) {
+6            if (res.isEmpty() || res.get(res.size() - 1) < n) {
+7                res.add(n);
+8            } else {
+9                int idx = binarySearch(res, n);
+10                res.set(idx, n);
+11            }
+12        }
+13
+14        return res.size();        
+15    }
+16
+17    private int binarySearch(List<Integer> arr, int target) {
+18        int left = 0;
+19        int right = arr.size() - 1;
+20
+21        while (left <= right) {
+22            int mid = (left + right) / 2;
+23            if (arr.get(mid) == target) {
+24                return mid;
+25            } else if (arr.get(mid) > target) {
+26                right = mid - 1;
+27            } else {
+28                left = mid + 1;
+29            }
+30        }
+31
+32        return left;
+33    }    
+34}
