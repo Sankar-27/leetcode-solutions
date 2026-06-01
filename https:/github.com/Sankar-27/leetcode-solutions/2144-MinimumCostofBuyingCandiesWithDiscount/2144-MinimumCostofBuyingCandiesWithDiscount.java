2d0bbcd2b0bab9bd5b924cec512cb92711ce3c57
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-06-01 10:00:24 +0530

    File updated at 01/06/2026, 10:00:25

--- https:/github.com/Sankar-27/leetcode-solutions/2144-MinimumCostofBuyingCandiesWithDiscount/2144-MinimumCostofBuyingCandiesWithDiscount.java
+++ https:/github.com/Sankar-27/leetcode-solutions/2144-MinimumCostofBuyingCandiesWithDiscount/2144-MinimumCostofBuyingCandiesWithDiscount.java
@@ -1,17 +1,18 @@
-// Last updated: 01/06/2026, 09:50:28
-1class Solution {
-2    public int minimumCost(int[] cost) {
-3        int min=0;
-4        Arrays.sort(cost);
-5        int count=2;
-6        for(int i=cost.length-1;i>=0;i--){
-7            if(count==0){
-8                count=2;
-9                continue;
-10            }
-11            min+=cost[i];
-12            count--;
-13        }
-14        return min;
-15    }
-16}
+// Last updated: 01/06/2026, 10:00:25
+class Solution {
+    public int minimumCost(int[] cost) {
+        PriorityQueue<Integer>pq = new PriorityQueue<Integer>(Comparator.reverseOrder());
+        for(int i=0;i<cost.length;i++){
+            pq.add(cost[i]);
+        }
+        int ans =0;
+        while(pq.size()>0){
+            int a = pq.poll();
+            int b = pq.size()>0 ? pq.poll() : 0;
+            int c = pq.size()>0 ? pq.poll() : 0;
+            ans+=a+b;
+        }
+        return ans;
+        
+    }
+}
