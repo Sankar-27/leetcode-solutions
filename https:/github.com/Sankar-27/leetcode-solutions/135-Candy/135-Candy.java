3183bf00e3972e5395af069596b093c34d0f25cb
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-06-02 12:36:35 +0530

    File updated at 02/06/2026, 12:36:35

--- https:/github.com/Sankar-27/leetcode-solutions/135-Candy/135-Candy.java
+++ https:/github.com/Sankar-27/leetcode-solutions/135-Candy/135-Candy.java
@@ -1,24 +1,24 @@
-// Last updated: 29/05/2026, 11:59:12
-class Solution {
-    public int candy(int[] a) {
-        int n=a.length;
-        if(n==0) return 0;
-       int[] c=new int[n];
-       Arrays.fill(c,1);
-       for(int i=1;i<n;i++){
-           if(a[i]>a[i-1]){
-               c[i]=c[i-1]+1;
-           }
-       }
-       for(int i=n-2;i>=0;i--){
-           if(a[i]>a[i+1]){
-               c[i]=Math.max(c[i],c[i+1]+1);
-           }
-       }
-       int res=0;
-       for(int i=0;i<n;i++){
-           res+=c[i];
-       }
-       return res;
-    }
-}
+// Last updated: 02/06/2026, 12:36:35
+1class Solution {
+2    public int candy(int[] a) {
+3        int n=a.length;
+4        if(n==0) return 0;
+5       int[] c=new int[n];
+6       Arrays.fill(c,1);
+7       for(int i=1;i<n;i++){
+8           if(a[i]>a[i-1]){
+9               c[i]=c[i-1]+1;
+10           }
+11       }
+12       for(int i=n-2;i>=0;i--){
+13           if(a[i]>a[i+1]){
+14               c[i]=Math.max(c[i],c[i+1]+1);
+15           }
+16       }
+17       int res=0;
+18       for(int i=0;i<n;i++){
+19           res+=c[i];
+20       }
+21       return res;
+22    }
+23}
