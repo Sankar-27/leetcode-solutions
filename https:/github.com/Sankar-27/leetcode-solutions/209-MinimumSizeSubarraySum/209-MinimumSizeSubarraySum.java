40c3a42092c3339421dbeb4444409bb347876a28
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-06-03 22:47:34 +0530

    File updated at 03/06/2026, 22:47:35

--- https:/github.com/Sankar-27/leetcode-solutions/209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.java
+++ https:/github.com/Sankar-27/leetcode-solutions/209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.java
@@ -1,22 +1,22 @@
-// Last updated: 29/05/2026, 11:58:21
-class Solution {
-    public int minSubArrayLen(int target, int[] nums) {
-        int left = 0, right = 0;
-        int sum = 0;
-        int minlen = Integer.MAX_VALUE;
-        
-        while (right < nums.length) {
-            sum += nums[right];
-            right++;
-            
-            while (sum >= target) {
-                int currentLen = right - left;
-                minlen = Math.min(minlen, currentLen);
-                sum -= nums[left];
-                left++;
-            }
-        }
-        
-        return minlen == Integer.MAX_VALUE ? 0 : minlen;
-    }
-}
+// Last updated: 03/06/2026, 22:47:35
+1class Solution {
+2    public int minSubArrayLen(int target, int[] nums) {
+3        int left = 0, right = 0;
+4        int sum = 0;
+5        int minlen = Integer.MAX_VALUE;
+6        
+7        while (right < nums.length) {
+8            sum += nums[right];
+9            right++;
+10            
+11            while (sum >= target) {
+12                int currentLen = right - left;
+13                minlen = Math.min(minlen, currentLen);
+14                sum -= nums[left];
+15                left++;
+16            }
+17        }
+18        
+19        return minlen == Integer.MAX_VALUE ? 0 : minlen;
+20    }
+21}
