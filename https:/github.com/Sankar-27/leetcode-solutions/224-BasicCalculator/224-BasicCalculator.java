a5be549deb5760c2dedc352110d33122ebccbded
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-06-03 23:09:53 +0530

    File updated at 03/06/2026, 23:09:54

--- https:/github.com/Sankar-27/leetcode-solutions/224-BasicCalculator/224-BasicCalculator.java
+++ https:/github.com/Sankar-27/leetcode-solutions/224-BasicCalculator/224-BasicCalculator.java
@@ -1,45 +1,45 @@
-// Last updated: 29/05/2026, 11:58:14
-class Solution {
-    public int calculate(String s) {
-
-        Stack<Integer> stack = new Stack<>();
-
-        int res = 0;
-        int curr = 0;
-        int sign = 1;
-
-        for (char c : s.toCharArray()) {
-
-            if (Character.isDigit(c)) {
-                curr = curr * 10 + (c - '0');
-            } 
-            else if (c == '+') {
-                res += curr * sign;
-                sign = 1;
-                curr = 0;
-            } 
-            else if (c == '-') {
-                res += curr * sign;
-                sign = -1;
-                curr = 0;
-            } 
-            else if (c == '(') {
-                stack.push(res);
-                stack.push(sign);
-                res = 0;
-                sign = 1;
-                curr = 0;
-            } 
-            else if (c == ')') {
-                res += curr * sign;
-                curr = 0;
-                res *= stack.pop();
-                res += stack.pop();
-            }
-            else if (c == ' ') continue;
-        }
-
-        res += sign * curr;
-        return res;
-    }
-}
+// Last updated: 03/06/2026, 23:09:54
+1class Solution {
+2    public int calculate(String s) {
+3
+4        Stack<Integer> stack = new Stack<>();
+5
+6        int res = 0;
+7        int curr = 0;
+8        int sign = 1;
+9
+10        for (char c : s.toCharArray()) {
+11
+12            if (Character.isDigit(c)) {
+13                curr = curr * 10 + (c - '0');
+14            } 
+15            else if (c == '+') {
+16                res += curr * sign;
+17                sign = 1;
+18                curr = 0;
+19            } 
+20            else if (c == '-') {
+21                res += curr * sign;
+22                sign = -1;
+23                curr = 0;
+24            } 
+25            else if (c == '(') {
+26                stack.push(res);
+27                stack.push(sign);
+28                res = 0;
+29                sign = 1;
+30                curr = 0;
+31            } 
+32            else if (c == ')') {
+33                res += curr * sign;
+34                curr = 0;
+35                res *= stack.pop();
+36                res += stack.pop();
+37            }
+38            else if (c == ' ') continue;
+39        }
+40
+41        res += sign * curr;
+42        return res;
+43    }
+44}
