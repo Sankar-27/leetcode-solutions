f1cab96add59fb2b7939542bfbd4a7809770d259
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-06-10 11:38:41 +0530

    File updated at 10/06/2026, 11:38:41

--- https:/github.com/Sankar-27/leetcode-solutions/73-SetMatrixZeroes/73-SetMatrixZeroes.java
+++ https:/github.com/Sankar-27/leetcode-solutions/73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -1,31 +1,32 @@
-// Last updated: 29/05/2026, 11:59:56
-class Solution {
-    public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean[] rowZero = new boolean[m];
-        boolean[] colZero = new boolean[n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    rowZero[i] = true;
-                    colZero[j] = true;
-                }
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            if (rowZero[i]) {
-                for (int j = 0; j < n; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        for (int j = 0; j < n; j++) {
-            if (colZero[j]) {
-                for (int i = 0; i < m; i++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-    }
-}
+// Last updated: 10/06/2026, 11:38:41
+1class Solution {
+2    public void setZeroes(int[][] matrix) {
+3        int m = matrix.length;
+4        int n = matrix[0].length;
+5        boolean[] rowZero = new boolean[m];
+6        boolean[] colZero = new boolean[n];
+7        for (int i = 0; i < m; i++) {
+8            for (int j = 0; j < n; j++) {
+9                if (matrix[i][j] == 0) {
+10                    rowZero[i] = true;
+11                    colZero[j] = true;
+12                }
+13            }
+14        }
+15        for (int i = 0; i < m; i++) {
+16            if (rowZero[i]) {
+17                for (int j = 0; j < n; j++) {
+18                    matrix[i][j] = 0;
+19                }
+20            }
+21        }
+22        for (int j = 0; j < n; j++) {
+23            if (colZero[j]) {
+24                for (int i = 0; i < m; i++) {
+25                    matrix[i][j] = 0;
+26                }
+27            }
+28        }
+29    }
+30}
+31
