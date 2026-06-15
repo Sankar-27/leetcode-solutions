86d18e2c97824629ad6d63143bdb66dfa129c880
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-06-15 22:55:19 +0530

    File updated at 15/06/2026, 22:55:19

--- https:/github.com/Sankar-27/leetcode-solutions/128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
+++ https:/github.com/Sankar-27/leetcode-solutions/128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
@@ -1,28 +1,28 @@
-// Last updated: 29/05/2026, 11:59:15
-class Solution {
-    public int longestConsecutive(int[] nums) {
-
-        int n = nums.length;
-        if (n == 0) return 0;
-        int longestLength = 1;
-
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            set.add(nums[i]);
-        }
-
-        for (int i : set) {
-            if (!set.contains(i - 1)) {
-                int count = 1;
-                int x = i;
-                while (set.contains(x + 1)) {
-                    x++;
-                    count++;
-                }
-                longestLength = Math.max(count, longestLength);
-            }
-        }
-
-        return longestLength;
-    }
-}
+// Last updated: 15/06/2026, 22:55:19
+1class Solution {
+2    public int longestConsecutive(int[] nums) {
+3
+4        int n = nums.length;
+5        if (n == 0) return 0;
+6        int longestLength = 1;
+7
+8        Set<Integer> set = new HashSet<>();
+9        for (int i = 0; i < n; i++) {
+10            set.add(nums[i]);
+11        }
+12
+13        for (int i : set) {
+14            if (!set.contains(i - 1)) {
+15                int count = 1;
+16                int x = i;
+17                while (set.contains(x + 1)) {
+18                    x++;
+19                    count++;
+20                }
+21                longestLength = Math.max(count, longestLength);
+22            }
+23        }
+24
+25        return longestLength;
+26    }
+27}
