d22657d5f6cace58276fec33d0b7410115b28c3f
Sankar-27 <166500157+Sankar-27@users.noreply.github.com>
2026-06-16 20:48:50 +0530

    File updated at 16/06/2026, 20:48:49

--- https:/github.com/Sankar-27/leetcode-solutions/394-DecodeString/394-DecodeString.java
+++ https:/github.com/Sankar-27/leetcode-solutions/394-DecodeString/394-DecodeString.java
@@ -1,31 +1,31 @@
-// Last updated: 29/05/2026, 11:57:21
-class Solution {
-    public String decodeString(String s) {
-        Stack<Integer> st = new Stack<>();
-        Stack<StringBuilder> st1 = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-        int n = 0;
-
-        for (char c : s.toCharArray()) {
-            if (Character.isDigit(c)) {
-                n = n * 10 + (c - '0');
-            } else if (c == '[') {
-                st.push(n);
-                n = 0;
-                st1.push(sb);
-                sb = new StringBuilder();
-            } else if (c == ']') {
-                int k = st.pop();
-                StringBuilder temp = sb;
-                sb = st1.pop();
-                while (k-- > 0) {
-                    sb.append(temp);
-                }
-            } else {
-                sb.append(c);
-            }
-        }
-
-        return sb.toString();
-    }
-} 
+// Last updated: 16/06/2026, 20:48:49
+1class Solution {
+2    public String decodeString(String s) {
+3        Stack<Integer> st = new Stack<>();
+4        Stack<StringBuilder> st1 = new Stack<>();
+5        StringBuilder sb = new StringBuilder();
+6        int n = 0;
+7
+8        for (char c : s.toCharArray()) {
+9            if (Character.isDigit(c)) {
+10                n = n * 10 + (c - '0');
+11            } else if (c == '[') {
+12                st.push(n);
+13                n = 0;
+14                st1.push(sb);
+15                sb = new StringBuilder();
+16            } else if (c == ']') {
+17                int k = st.pop();
+18                StringBuilder temp = sb;
+19                sb = st1.pop();
+20                while (k-- > 0) {
+21                    sb.append(temp);
+22                }
+23            } else {
+24                sb.append(c);
+25            }
+26        }
+27
+28        return sb.toString();
+29    }
+30} 
